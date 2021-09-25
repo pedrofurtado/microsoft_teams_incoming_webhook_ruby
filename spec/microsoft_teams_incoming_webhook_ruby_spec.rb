78803901c93e616aafb24c09ace82020bcb8f133
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-09-25 21:12:02 +0000

    Docs examples

--- spec/microsoft_teams_incoming_webhook_ruby_spec.rb
+++ spec/microsoft_teams_incoming_webhook_ruby_spec.rb
@@ -9,80 +9,80 @@ RSpec.describe MicrosoftTeamsIncomingWebhookRuby do
 end
 
 RSpec.describe MicrosoftTeamsIncomingWebhookRuby::Message do
-	context '#initialize' do
-		it 'constructs an OpenStruct object from the given block' do
-			message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-				m.url             = 'https://example.com'
-				m.title           = 'Hello World'
-				m.text            = '1 2 3'
-				m.arbitrary_field = 'An arbitrary field'
-			end
+  context '#initialize' do
+    it 'constructs an OpenStruct object from the given block' do
+      message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+        m.url             = 'https://example.com'
+        m.title           = 'Hello World'
+        m.text            = '1 2 3'
+        m.arbitrary_field = 'An arbitrary field'
+      end
 
-			expect(message).to_not be_nil
-			expect(message.builder).to_not be_nil
-			expect(message.builder).to be_a(OpenStruct)
-			expect(message.builder.url).to eq('https://example.com')
-			expect(message.builder.title).to eq('Hello World')
-			expect(message.builder.text).to eq('1 2 3')
-			expect(message.builder.arbitrary_field).to eq('An arbitrary field')
-		end
+      expect(message).to_not be_nil
+      expect(message.builder).to_not be_nil
+      expect(message.builder).to be_a(OpenStruct)
+      expect(message.builder.url).to eq('https://example.com')
+      expect(message.builder.title).to eq('Hello World')
+      expect(message.builder.text).to eq('1 2 3')
+      expect(message.builder.arbitrary_field).to eq('An arbitrary field')
+    end
 
-		it 'requires a block to be initialized' do
-			expect { MicrosoftTeamsIncomingWebhookRuby::Message.new }.to raise_error(LocalJumpError)
-		end
+    it 'requires a block to be initialized' do
+      expect { MicrosoftTeamsIncomingWebhookRuby::Message.new }.to raise_error(LocalJumpError)
+    end
 
-		it 'raises an InvalidMessage when the builder object is initialized without a url property' do
+    it 'raises an InvalidMessage when the builder object is initialized without a url property' do
       error_message = "'url' must be defined in Message"
 
-			expect do
-				MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url  = nil
-					m.text = '1 2 3'
-				end
-			end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
+      expect do
+        MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url  = nil
+          m.text = '1 2 3'
+        end
+      end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
 
-			expect do
-				MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = ''
-					m.text = '1 2 3'
-				end
-			end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
+      expect do
+        MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = ''
+          m.text = '1 2 3'
+        end
+      end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
 
-			expect do
-				MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.text = '1 2 3'
-				end
-			end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
-		end
+      expect do
+        MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.text = '1 2 3'
+        end
+      end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
+    end
 
-		it 'raises an InvalidMessage when the `builder` object is initialized without a text property' do
+    it 'raises an InvalidMessage when the `builder` object is initialized without a text property' do
       error_message = "'text' must be defined in Message"
 
-			expect do
-				MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = nil
-				end
-			end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
+      expect do
+        MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = nil
+        end
+      end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
 
-			expect do
-				MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = ''
-				end
-			end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
+      expect do
+        MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = ''
+        end
+      end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
 
-			expect do
-				MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-				end
-			end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
-		end
-	end
+      expect do
+        MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+        end
+      end.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage, error_message)
+    end
+  end
 
-	context '#send' do
-		context 'with a valid builder object' do
-			it 'returns Net::HTTPOK' do
+  context '#send' do
+    context 'with a valid builder object' do
+      it 'returns Net::HTTPOK' do
         [200, 300].each do |successful_http_code|
           message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
             m.url = 'https://example.com/example'
@@ -104,7 +104,7 @@ RSpec.describe MicrosoftTeamsIncomingWebhookRuby::Message do
 
           expect(message.send.response.code.to_i).to eq(successful_http_code)
         end
-			end
+      end
 
       it 'returns FailedRequest if any error occurs' do
         [100, 400, 500].each do |non_successful_http_code|
@@ -128,66 +128,66 @@ RSpec.describe MicrosoftTeamsIncomingWebhookRuby::Message do
 
           expect { message.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::FailedRequest)
         end
-			end
-		end
+      end
+    end
 
-		context 'with an invalid builder object' do
-			it 'does raise an error without a set url property' do
-				message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = '1 2 3'
-				end
-				message.builder.delete_field(:url)
-				expect { message.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
+    context 'with an invalid builder object' do
+      it 'does raise an error without a set url property' do
+        message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = '1 2 3'
+        end
+        message.builder.delete_field(:url)
+        expect { message.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
 
-				message_2 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = '1 2 3'
-				end
-				message_2.builder.url = ''
-				expect { message_2.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
+        message_2 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = '1 2 3'
+        end
+        message_2.builder.url = ''
+        expect { message_2.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
 
-				message_3 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = '1 2 3'
-				end
-				message_3.builder.url = nil
-				expect { message_3.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
-			end
+        message_3 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = '1 2 3'
+        end
+        message_3.builder.url = nil
+        expect { message_3.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
+      end
 
-			it 'does raise an error without a set text property' do
-				message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = '1 2 3'
-				end
-				message.builder.delete_field(:text)
-				expect { message.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
+      it 'does raise an error without a set text property' do
+        message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = '1 2 3'
+        end
+        message.builder.delete_field(:text)
+        expect { message.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
 
-				message_2 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = '1 2 3'
-				end
-				message_2.builder.text = ''
-				expect { message_2.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
+        message_2 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = '1 2 3'
+        end
+        message_2.builder.text = ''
+        expect { message_2.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
 
-				message_3 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-					m.url = 'https://example.com'
-					m.text = '1 2 3'
-				end
-				message_3.builder.text = nil
-				expect { message_3.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
-			end
-		end
-	end
+        message_3 = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+          m.url = 'https://example.com'
+          m.text = '1 2 3'
+        end
+        message_3.builder.text = nil
+        expect { message_3.send }.to raise_error(MicrosoftTeamsIncomingWebhookRuby::Message::Error::InvalidMessage)
+      end
+    end
+  end
 
   context '#builder' do
     it 'cannot be redefined itself' do
-			message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-				m.url             = 'https://example.com'
-				m.title           = 'Hello World'
-				m.text            = '1 2 3'
-				m.arbitrary_field = 'An arbitrary field'
-			end
+      message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+        m.url             = 'https://example.com'
+        m.title           = 'Hello World'
+        m.text            = '1 2 3'
+        m.arbitrary_field = 'An arbitrary field'
+      end
 
       expect { message.builder = nil }.to raise_error(NoMethodError)
       expect { message.builder = '' }.to raise_error(NoMethodError)
@@ -195,12 +195,12 @@ RSpec.describe MicrosoftTeamsIncomingWebhookRuby::Message do
     end
 
     it 'can redefine properties lately' do
-			message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-				m.url             = 'https://example.com'
-				m.title           = 'Hello World'
-				m.text            = '1 2 3'
-				m.arbitrary_field = 'An arbitrary field'
-			end
+      message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+        m.url             = 'https://example.com'
+        m.title           = 'Hello World'
+        m.text            = '1 2 3'
+        m.arbitrary_field = 'An arbitrary field'
+      end
 
       expect(message.builder.arbitrary_field).to eq 'An arbitrary field'
 
@@ -210,12 +210,12 @@ RSpec.describe MicrosoftTeamsIncomingWebhookRuby::Message do
     end
 
     it 'can add properties lately' do
-			message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-				m.url             = 'https://example.com'
-				m.title           = 'Hello World'
-				m.text            = '1 2 3'
-				m.arbitrary_field = 'An arbitrary field'
-			end
+      message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+        m.url             = 'https://example.com'
+        m.title           = 'Hello World'
+        m.text            = '1 2 3'
+        m.arbitrary_field = 'An arbitrary field'
+      end
 
       expect(message.builder.respond_to?(:another_field)).to eq false
       expect(message.builder.another_field).to eq nil
@@ -227,12 +227,12 @@ RSpec.describe MicrosoftTeamsIncomingWebhookRuby::Message do
     end
 
     it 'can remove properties lately' do
-			message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
-				m.url             = 'https://example.com'
-				m.title           = 'Hello World'
-				m.text            = '1 2 3'
-				m.arbitrary_field = 'An arbitrary field'
-			end
+      message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+        m.url             = 'https://example.com'
+        m.title           = 'Hello World'
+        m.text            = '1 2 3'
+        m.arbitrary_field = 'An arbitrary field'
+      end
 
       expect(message.builder.respond_to?(:arbitrary_field)).to eq true
       expect(message.builder.arbitrary_field).to eq 'An arbitrary field'
